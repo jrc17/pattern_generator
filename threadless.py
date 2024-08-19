c4f19c9a2fd06ac09e7e875df6cc988a1eda38ab
Jayanthi <jayanthi.chesetti@gmail.com>
2024-08-19 02:40:52 -0600

    made code more modular

--- threadless.py
+++ threadless.py
@@ -1,6 +1,17 @@
 from PIL import Image
 
-filename = "yellow.png"
+filename = "orange.png"
+
+def tiled(tile, pattern_width, pattern_height,type):
+        # make a pattern 
+        filename = type+"_pattern.jpg"
+        pattern = Image.new("RGB",(pattern_width,pattern_height))
+
+        for y in range(0,pattern_height,tile.height):
+            for x in range(0,pattern_width,tile.width):
+                pattern.paste(tile,(x,y))
+        
+        pattern.save(filename)
 
 with Image.open(filename) as tile:
     tile.load()
@@ -12,26 +23,20 @@ with Image.open(filename) as tile:
 
     resize_tile_500 = tile.resize((500,500))
     resize_tile_500.save("tile_500.png")
-
-
-    def tiled(tile, pattern_width, pattern_height,type):
-        # make a pattern 
-        filename = type+"_pattern.jpg"
-        pattern = Image.new("RGB",(pattern_width,pattern_height))
-
-        for y in range(0,pattern_height,tile.height):
-            for x in range(0,pattern_width,tile.width):
-                pattern.paste(tile,(x,y))
-        
-        pattern.save(filename)
-        
-
  
 
     tiled(resize_tile_2000, 13000, 13000,"main" )
-    tiled(resize_tile_500, 9038, 8148,"shirt" )
-    tiled(resize_tile_500, 5400, 6374,"socks" )
-    tiled(resize_tile_500, 5968, 5709,"shoes" )
-    tiled(resize_tile_500, 8250, 6415,"leggings" )
-    tiled(resize_tile_500, 4050, 7800,"duffle" )
-    tiled(resize_tile_500, 3900, 4575,"bag" )
+
+# for now this works as the code is small 
+#if i want to expand it , it might be better to generilize it or something and add metadata
+    
+    patterns = [(9038, 8148,"shirt"),
+                ( 5400, 6374,"socks"),
+                (5968, 5709,"shoes"),
+                (8250, 6415,"leggings"),
+                (4050, 7800,"duffle"),
+                (3900, 4575,"bag")
+                ]
+
+    for w,h,type in patterns: tiled(resize_tile_500, w,h,type)
+
